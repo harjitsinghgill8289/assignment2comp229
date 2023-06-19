18e2f19e8c1468276b3ce194f4cb3374a66298e7
harjitsinghgill8289 <128982764+harjitsinghgill8289@users.noreply.github.com>
2023-06-18 22:41:28 -0400

    Initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    //url: "mongodb://localhost:27017/Blessing_db"
+  
+   
+  
+    //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+  
+     //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+  
+  //url:"mongodb+srv://harjitgill14367:dVYibbbUIzBVlTgd@cluster0.i45yip8.mongodb.net/SportStore?retryWrites=true&w=majority"
+  url:"mongodb+srv://harjitgill14367:dVYibbbUIzBVlTgd@cluster0.i45yip8.mongodb.net/DressStore?retryWrites=true&w=majority"
+  };
+  
+   
